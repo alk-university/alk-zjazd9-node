0376598f9febed79f0f210c12448a0bce540d4d5
medynski <wojciech@medynski.io>
2020-06-20 11:29:13 +0200

    Working login endpoint

--- server.js
+++ server.js
@@ -7,11 +7,34 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const httpServer = http.Server(app);
 
+// base64 helpers
+const btoa = (string) => Buffer.from(string).toString('base64');
+const atob = (encoded) => Buffer.from(encoded, 'base64').toString();
+
 const db = {};
 
 db.posts = Datastore({
   filename: path.resolve(path.dirname(''), './database/posts.db'),
   autoload: true,
+});
+
+db.users = Datastore({
+  filename: path.resolve(path.dirname(''), './database/users.db'),
+  autoload: true,
+});
+
+app.get('/api/db', async (req, res) => {
+  // const user = await db.users.insert({
+  //   userName: 'moderator',
+  //   token: btoa('moderator:password'),
+  // });
+  const token = 'YWRtaW46cGFzc3dvcmQ=';
+  const user = await db.users.findOne({ token });
+  if (user !== null) {
+    res.json({ authorized: true });
+  } else {
+    res.status(401).json({ authorized: false });
+  }
 });
 
 app.use(express.json()); // for parsing application/json
@@ -22,13 +45,13 @@ app.get('/api', (req, res) => {
 });
 
 // user authentication
-app.post('/api/login', (req, res) => {
-  if (req.body.token) {
-    if (req.body.token === 'YWRtaW46cGFzc3dvcmQ=') {
-      res.json({ authorized: true });
-    } else {
-      res.status(401).json({ authorized: false });
-    }
+app.post('/api/login', async (req, res) => {
+  const { token } = req.body;
+  const user = await db.users.findOne({ token });
+  if (user !== null) {
+    res.json({ authorized: true });
+  } else {
+    res.status(401).json({ authorized: false });
   }
 });
 
